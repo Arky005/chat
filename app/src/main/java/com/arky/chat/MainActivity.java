ddf2f4aa36ff2ada722e42a0454641e32717e3cf
Arky005 <otavio.n.morais@hotmail.com>
2018-10-29 14:02:22 -0200

    alteracao no id

--- app/src/main/java/com/arky/chat/MainActivity.java
+++ app/src/main/java/com/arky/chat/MainActivity.java
@@ -23,23 +23,20 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
 
-    private Context mContext;
-    Point size;
     LinearLayout layout;
     ScrollView scrow;
     Button enviar;
+    Point size;
+    int idGlobal=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         size = new Point();
         getWindowManager().getDefaultDisplay().getSize(size);
 
-        mContext=getApplicationContext();
         Random rand = new Random();
-
 
         layout = (LinearLayout)findViewById(R.id.layoutscroll);
         scrow = (ScrollView)findViewById(R.id.scrow);
@@ -54,7 +51,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 if(!escrita.getText().toString().equals("")) {
-                    layout.addView(new mensagem(escrita.getText().toString(), 1, true).getView());
+                    new mensagem(escrita.getText().toString(), true).criar();
                     escrita.setText("");
                     scrow.scrollTo(0, scrow.getBottom());
                 }
@@ -63,7 +60,7 @@ public class MainActivity extends Activity {
 
 
 
-        new mensagem("Oi! Tudo bem? Como vai a familia?", 0, false).criar();
+        new mensagem("Oi! Tudo bem? Como vai a familia?", false).criar().setCor(Color.RED);
         //layout.addView( new mensagem("Sim! Tudo bem graças a Deus!", 1, true).getView() );
 
 
@@ -71,7 +68,7 @@ public class MainActivity extends Activity {
 
 
     public Context getContext(){
-        return mContext;
+        return getApplicationContext();
     }
 
     //Classe para criar mensagens personalizadas
@@ -81,20 +78,18 @@ public class MainActivity extends Activity {
         private int id, cor, tamanho;
         private boolean usuario;
 
-        public mensagem(String texto, int id, boolean usuario){
+        public mensagem(String texto, boolean usuario){
 
 
             this.texto=new TextView(getContext());
-
             this.texto.setTextIsSelectable(true); //pode copiar as mensagens?
             this.setTexto(texto);
             this.setCor(Color.BLACK);
-            this.setId(id);
+            this.setId(idGlobal);
             this.setTamanho(17);
             this.setUsuario(usuario);
             this.texto.setHeight(80+(this.texto.getText().toString().length()*2));
             this.texto.setY(20);
-
 
             //muda o layout dos textos pra quebrar as linhas depois do 15 caracter
             // this.texto.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -105,17 +100,18 @@ public class MainActivity extends Activity {
                 this.texto.setWidth(size.x-(size.x/4));
             } else {
                 this.texto.setGravity(Gravity.LEFT);
-               // this.texto.setBackgroundColor(Color.LTGRAY);
             }
+
+            idGlobal++;
         }
 
-        public mensagem(String texto, int id, int cor, int tamanho, boolean usuario){
+        public mensagem(String texto, int cor, int tamanho, boolean usuario){
             this.texto=new TextView(getContext());
 
             this.texto.setTextIsSelectable(true); //pode copiar as mensagens?
             this.setTexto(texto);
             this.setCor(cor);
-            this.setId(id);
+            this.setId(idGlobal);
             this.setTamanho(tamanho);
             this.setUsuario(usuario);
 
@@ -131,10 +127,12 @@ public class MainActivity extends Activity {
                 // this.texto.setBackgroundColor(Color.LTGRAY);
             }
 
+            idGlobal++;
         }
 
-        public void criar(){
+        public mensagem criar(){
             layout.addView(texto);
+            return this;
         }
         public TextView getView() {
             return texto;
