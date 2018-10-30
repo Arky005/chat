5940f75b779d1282a666afca83ec1f1d60e559d2
Arky005 <otavio.n.morais@hotmail.com>
2018-10-30 10:59:17 -0200

    alteracao no tamanho da mensagem

--- app/src/main/java/com/arky/chat/MainActivity.java
+++ app/src/main/java/com/arky/chat/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.text.InputType;
@@ -58,43 +59,33 @@ public class MainActivity extends Activity {
             }
         });
 
-        new mensagem("Oi! Tudo bem? Como vai a familia?", false).criar().setCor(Color.RED);
-        //layout.addView( new mensagem("Sim! Tudo bem graças a Deus!", 1, true).getView() );
+        new mensagem("Oi! Tudo bem? Como vai a familia?", false).criar();
 
-    }
-
-
-    public Context getContext(){
-        return getApplicationContext();
     }
 
     //Classe para criar mensagens personalizadas
+    //cada mensagem pode ter no maximo 1 linha(25 caracteres).
     class mensagem {
 
         private TextView texto;
-        private int id, cor, tamanho;
+        private int id, cor, tamanhoFonte;
         private boolean usuario;
 
         public mensagem(String texto, boolean usuario){
 
 
-            this.texto=new TextView(getContext());
+            this.texto=new TextView(getApplicationContext());
             this.texto.setTextIsSelectable(true); //pode copiar as mensagens?
             this.setTexto(texto);
             this.setCor(Color.BLACK);
             this.setId(idGlobal);
-            this.setTamanho(17);
+            this.setTamanhoFonte(17);
             this.setUsuario(usuario);
-            this.texto.setHeight(80+(this.texto.getText().toString().length()*2));
-            this.texto.setY(20);
+            this.texto.setHeight( this.getTamanhoFonte()*5*numeroLinhas() );
 
-            //muda o layout dos textos pra quebrar as linhas depois do 15 caracter
-            // this.texto.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            //this.texto.setMaxEms(15); //quebra a linha depois do 15 caracter
 
             if(usuario){
                 this.texto.setGravity(Gravity.RIGHT);
-                this.texto.setWidth(size.x-(size.x/4));
             } else {
                 this.texto.setGravity(Gravity.LEFT);
             }
@@ -103,18 +94,15 @@ public class MainActivity extends Activity {
         }
 
         public mensagem(String texto, int cor, int tamanho, boolean usuario){
-            this.texto=new TextView(getContext());
+            this.texto=new TextView(getApplicationContext());
 
             this.texto.setTextIsSelectable(true); //pode copiar as mensagens?
             this.setTexto(texto);
             this.setCor(cor);
             this.setId(idGlobal);
-            this.setTamanho(tamanho);
+            this.setTamanhoFonte(tamanho);
             this.setUsuario(usuario);
 
-            //muda o layout dos textos pra quebrar as linhas depois do 15 caracter
-            // this.texto.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            //this.texto.setMaxEms(15); //quebra a linha depois do 15 caracter
 
             if(usuario){
                 this.texto.setGravity(Gravity.RIGHT);
@@ -127,32 +115,46 @@ public class MainActivity extends Activity {
             idGlobal++;
         }
 
+        private int numeroLinhas(){
+            int resp=1;
+            int tamanho=this.texto.getText().length();
+            if(tamanho>25&&tamanho<50){
+                resp=2;
+            }
+            return resp;
+        }
+
         public mensagem criar(){
             layout.addView(texto);
             return this;
         }
         public TextView getView() {
+
             return texto;
         }
 
         public String getTexto(){
+
             return ""+texto.getText();
         }
 
         public boolean isUsuario() {
+
             return usuario;
         }
 
         public int getCor() {
+
             return cor;
         }
 
         public int getId() {
+
             return id;
         }
 
-        public int getTamanho() {
-            return tamanho;
+        public int getTamanhoFonte() {
+            return tamanhoFonte;
         }
 
         public void setCor(int cor) {
@@ -161,16 +163,18 @@ public class MainActivity extends Activity {
         }
 
         public void setUsuario(boolean usuario) {
+
             this.usuario = usuario;
         }
 
         public void setId(int id) {
+
             this.id = id;
         }
 
-        public void setTamanho(int tamanho) {
+        public void setTamanhoFonte(int tamanho) {
             this.texto.setTextSize(tamanho);
-            this.tamanho = tamanho;
+            this.tamanhoFonte = tamanho;
         }
 
         public void setTexto(String texto) {
